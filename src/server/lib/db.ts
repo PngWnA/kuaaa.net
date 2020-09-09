46d00d442df49cc1463530b8d6687ab08054f01e
PngWnA <2016320146@korea.ac.kr>
2020-09-09 13:06:48 +0900

    [Server::ORM] Modified import path

--- src/server/lib/db.ts
+++ src/server/lib/db.ts
@@ -5,8 +5,7 @@ import { Sequelize } from "sequelize";
 const db: any = {};
 const sequelize: Sequelize = new Sequelize('sqlite::memory:');
 
-fs.readdirSync(__dirname)
-.filter((file) => { return (file.indexOf('.') !== 0) && (file !== 'index.js'); })
+fs.readdirSync(path.join(__dirname, `model`))
 .forEach((file) => { const model = sequelize.import(path.join(__dirname, file)); db[model.name] = model; });
 
 db.sequelize = sequelize;

ecf8dcd3fa7dec655215f9a5cb46356aeda39eef
PngWnA <2016320146@korea.ac.kr>
2020-09-16 11:40:09 +0900

    [Hotfix] temp save

---
+++ src/server/lib/auth.ts
@@ -0,0 +1,42 @@
+import { Sequelize } from "sequelize/types";
+
+export default (sequelize: Sequelize, DataTypes: any) => {
+    const Users = sequelize.define('Users', {
+        uid: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        sid: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        nickname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: '',
+        },
+        email: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0,
+        },
+        isActive: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: 1,
+        },
+    });
+
+    Users.associate = models => {
+        Users.hasMany(models.Boards);
+        Users.hasMany(models.Articles);
+        Users.hasMany(models.Comments);
+    };
+
+    return Users;
+};
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,13 @@
 import Express from 'express';
+import db from './lib/db.js';
 import router from './route/router.js';
 
 const app: Express.Application = Express();
-
 app.use('/', router);
+
+db.sequelize
+.sync()
+.then(() => {console.log(`DB Sync done.`)})
+.catch(() => {console.log(`WTF?`)});
 
 export default app;

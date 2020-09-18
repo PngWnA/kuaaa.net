e0d0fd5c8949991d524911fbae4ceafcdfcb3dee
PngWnA <2016320146@korea.ac.kr>
2020-09-18 14:22:57 +0900

    [Hotfix] typo

--- src/server/init.js
+++ src/server/init.js
@@ -2,4 +2,4 @@ const server = require('./server');
 
 const port = Number(process.env.PORT) || 31413;
 
-server.listen(port, () => { console.log(`Server is now listening on ${port}!`) });
+server.listen(port, () => { console.log(`Server is now listening on ::${port}!`) });

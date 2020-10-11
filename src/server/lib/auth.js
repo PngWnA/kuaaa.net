451459503237b78b4648b0753c1f0deec3c8270a
PngWnA <2016320146@korea.ac.kr>
2020-10-11 16:03:34 +0900

    [Auth] Implemented jwt issue/verify

--- src/server/lib/auth.js
+++ src/server/lib/auth.js
@@ -1,19 +1,22 @@
 const crypto = require('crypto');
 const util = require('util');
+const jwt = require('jsonwebtoken');
 
 const pbkdf2 = util.promisify(crypto.pbkdf2);
 const randomBytes = util.promisify(crypto.randomBytes);
 const KEY_LEN = 512;
 
-async function generatePassword(password) {
+const jwtSecret = process.env.JWT_SECRET || 'DEV_TEST';
+
+const generatePassword = async (password) => {
   const ITER = 123456;
   const ALGO = 'sha512';
   const salt = await randomBytes(16);
   const digest = await pbkdf2(password, salt, ITER, KEY_LEN, ALGO);
   return `${ALGO}:${salt.toString('base64')}:${ITER}:${KEY_LEN}:${digest.toString('base64')}`;
-}
+};
 
-async function comparePassword(password, storedPassword) {
+const comparePassword = async (password, storedPassword) => {
   const [algo, encodedSalt, iterStr, keylenStr, encodedDigest] = storedPassword.split(':');
   const iter = parseInt(iterStr, 10);
   const keylen = parseInt(keylenStr, 10);
@@ -21,7 +24,31 @@ async function comparePassword(password, storedPassword) {
   const digest = Buffer.from(encodedDigest, 'base64');
   const hashed = await pbkdf2(password, salt, iter, keylen, algo);
   return Buffer.compare(hashed, digest) === 0;
-}
+};
+
+const issueToken = async (user) => {
+  const token = jwt.sign(
+    {
+      uid: user.uid,
+      nickname: user.nickname,
+      level: user.level,
+    },
+    jwtSecret,
+    {
+      expiresIn: '7d',
+      issuer: 'kuaaa.net',
+      subject: 'Auth Token',
+    },
+  );
+  return token;
+};
+
+const verifyToken = async (token) => {
+  const verified = jwt.verify(token);
+  return verified;
+};
 
 generatePassword('a');
 comparePassword('b');
+issueToken('user');
+verifyToken('a');

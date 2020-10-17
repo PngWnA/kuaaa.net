4cd49ffd89a3918e94125c114ac82179265d4ce3
PngWnA <2016320146@korea.ac.kr>
2020-10-17 21:38:04 +0900

    [Account] Implemented login endpoint

--- src/server/router/account/account.js
+++ src/server/router/account/account.js
@@ -1,5 +1,5 @@
 const { Users, Auth } = require('../../lib/db');
-const { generatePassword } = require('../../lib/auth');
+const { generatePassword, issueToken, comparePassword } = require('../../lib/auth');
 
 const test = async (req, res, next) => {
   res.send('GET /account/');
@@ -41,4 +41,47 @@ const register = async (req, res, next) => {
   return next();
 };
 
-module.exports = { test, register };
+const login = async (req, res, next) => {
+  const { id, pw } = req.body;
+  if (!id || !pw) {
+    res.status(400).json({ id: id ? 'OK' : null, pw: pw ? 'OK' : null });
+  }
+
+  const user = await Users.findOne({
+    where: {
+      id,
+      isActive: 1,
+    },
+    attributes: ['uid', 'nickname'],
+  });
+
+  if (!user) {
+    res.status(401).json({});
+    return next();
+  }
+
+  const auth = await Auth.findOne({
+    where: {
+      uid: user.uid,
+    },
+    attributes: ['digest', 'level'],
+  });
+
+  if (!auth) {
+    res.status(401).json({});
+    return next();
+  }
+
+  if (await comparePassword(pw, auth.digest) === false) {
+    res.status(401).json({});
+    return next();
+  }
+
+  user.level = auth.level;
+
+  const jwt = await issueToken(user);
+  res.status(200).json({ token: jwt });
+  return next();
+};
+
+module.exports = { test, register, login };
--- src/server/router/account/index.js
+++ src/server/router/account/index.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-const { test, register } = require('./account');
+const { test, register, login } = require('./account');
 
 const router = Router();
 
 router.get('/', test);
 router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;

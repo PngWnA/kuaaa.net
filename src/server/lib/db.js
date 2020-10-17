df993256a7e598f82c43751a2ba28a2e7829569e
PngWnA <2016320146@korea.ac.kr>
2020-10-17 19:20:07 +0900

    [Auth] Added Expired table to schema

--- src/server/lib/db.js
+++ src/server/lib/db.js
@@ -12,5 +12,6 @@ db.Auth = require('./model/auth')(sequelize, Sequelize.DataTypes);
 db.Articles = require('./model/articles')(sequelize, Sequelize.DataTypes);
 db.Comments = require('./model/comments')(sequelize, Sequelize.DataTypes);
 db.Boards = require('./model/boards')(sequelize, Sequelize.DataTypes);
+db.Expired = require('./model/expired')(sequelize, Sequelize.DataTypes);
 
 module.exports = db;
